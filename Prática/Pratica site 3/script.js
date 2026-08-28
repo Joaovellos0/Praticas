5361cfdb34d45125115a0cf7a6de6f3717eb75ad
Joaovellos0 <jpmarianovelloso@gmail.com>
2026-08-28 16:29:31 -0300

    pratica

---
+++ Prática/Pratica site 3/script.js
@@ -0,0 +1,3 @@
+const botaoLigar = document.querySelector("sidebarToggleOn")
+const sidebar = document.querySelector("sidebar fechada")
+const botaoDesligar = document.querySelector("sidebarToggleOff")
